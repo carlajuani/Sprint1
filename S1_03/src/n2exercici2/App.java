e4a80fa7f5e381b778d2fa226c08b0905f9e90e3
carlajuani <carla90_@hotmail.es>
2022-11-24 13:27:43 +0100

    Acabat

--- S1_03/src/n2exercici2/App.java
+++ S1_03/src/n2exercici2/App.java
@@ -1,5 +1,9 @@
 package n2exercici2;
-/*  */
+/* Utilitzant la classe del programa anterior, crea la implementació necessària perquè els objectes de la classe Restaurant 
+ * estiguin ordenats per nom i per puntuació en ordre ascendent. 
+Exemple: 
+nom: restaurant1, puntuació: 8
+nom: restaurant1, puntuació: 7 */
 import java.util.*;
 
 public class App {
